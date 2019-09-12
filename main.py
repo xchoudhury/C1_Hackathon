65fcc38f25393c756e432500fad19f02a9f30672
Xavier Choudhury <lyx163@38f9d3c01fab.cof.ds.capitalone.com>
2019-09-12 10:50:30 -0400

    create bill & create depost done

--- main.py
+++ main.py
@@ -57,10 +57,10 @@ def create_customer(payload):
 def create_credit_account(customerId):
     url = base_url + '/customers/{}/accounts'.format(customerId)
     payload = {
-    "type": "Credit Card",
-    "nickname": "Savor",
-    "rewards": 500,
-    "balance": 300,
+        "type": "Credit Card",
+        "nickname": "Savor",
+        "rewards": 500,
+        "balance": 300,
     }
     # Create a Credit Card Account
     resp = requests.post( 
@@ -79,12 +79,56 @@ def create_credit_account(customerId):
 def create_bill(accountID):
     url = base_url + '/accounts/{}/bills'.format(accountID)
     payload = {
-    'status' : 'pending',
-    'payee' : 'Kaido Enterprise',
-    'payment_amount' : 1738.42
+        'status' : 'pending',
+        'payee' : 'Kaido Enterprise',
+        'payment_amount' : 1738.42
     }
 
-    # Create a Credit Card Account
+    # Create a bill
+    resp = requests.post( 
+        url, 
+        data=json.dumps(payload),
+        headers={'content-type':'application/json'},
+        params={'key': apiKey}
+        )
+
+    if not resp.ok:
+        print(resp.reason)
+        return resp.json()
+    
+    return resp.json()['objectCreated']
+
+def create_purchase(accountID):
+    url = base_url + '/accounts/{}/purchases'.format(accountID)
+    payload = {
+        "merchant_id": "57cf75cea73e494d8675ec4b", # Apple Store
+        "medium": "balance",
+        "amount": 299.99,
+        "status": "pending",
+    }
+
+    # Create a purchase
+    resp = requests.post( 
+        url, 
+        data=json.dumps(payload),
+        headers={'content-type':'application/json'},
+        params={'key': apiKey}
+        )
+
+    if not resp.ok:
+        print(resp.reason)
+        return resp.json()
+    
+    return resp.json()['objectCreated']
+
+def create_deposit(accountID):
+    url = base_url + '/accounts/{}/deposits'.format(accountID)
+    payload = {
+        "medium": "balance",
+        "amount": 100.00
+    }
+
+    # Create a deposit
     resp = requests.post( 
         url, 
         data=json.dumps(payload),
@@ -129,6 +173,14 @@ def main():
     # Create a bill for said credit card
     bill = create_bill(credit_acc['_id'])
     print(bill)
+
+    # Create a purchase for said credit card
+    purchase = create_purchase(credit_acc['_id'])
+    print(purchase)
+
+    # Create a deposit for said credit card
+    deposit = create_deposit(credit_acc['_id'])
+    print(deposit)
 
 if __name__ == "__main__":
     main()
