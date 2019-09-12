dbb6c2c5b5ecb7c03030340490cf3e0bbba995ad
Xavier Choudhury <lyx163@38f9d3c01fab.cof.ds.capitalone.com>
2019-09-12 11:02:26 -0400

    Code cleanup

--- main.py
+++ main.py
@@ -5,6 +5,28 @@ customerId = '5d72722a322fa016762f2f96'
 apiKey = '3307ed298dcffa965a1c7fc50085ef47'
 base_url = 'http://api.reimaginebanking.com'
 
+def make_post(url, payload):
+    headers={
+        'content-type':'application/json'
+    }
+
+    params = {
+        'key' : apiKey,
+    }
+
+    resp = requests.post(
+        url,
+        headers=headers,
+        params=params,
+        data=json.dumps(payload)
+    )
+
+    if not resp.ok:
+        print(resp.reason)
+        return resp.json()
+    
+    return resp.json()['objectCreated']
+
 def get_atms(lat=0, lng=0, rad=1):
     url = base_url + '/atms'
     params = {
@@ -36,23 +58,7 @@ def get_atms(lat=0, lng=0, rad=1):
 
 def create_customer(payload):
     url = base_url + '/customers'
-
-    params = {
-        'key' : apiKey,
-    }
-
-    resp = requests.post(
-        url,
-        headers={'content-type':'application/json'},
-        params=params,
-        data=json.dumps(payload)
-    )
-
-    if not resp.ok:
-        print(resp.reason)
-        return resp.json()
-    
-    return resp.json()['objectCreated']
+    return make_post(url, payload)
 
 def create_credit_account(customerId):
     url = base_url + '/customers/{}/accounts'.format(customerId)
@@ -62,19 +68,9 @@ def create_credit_account(customerId):
         "rewards": 500,
         "balance": 300,
     }
-    # Create a Credit Card Account
-    resp = requests.post( 
-        url, 
-        data=json.dumps(payload),
-        headers={'content-type':'application/json'},
-        params={'key': apiKey}
-        )
 
-    if not resp.ok:
-        print(resp.reason)
-        return resp.json()
-    
-    return resp.json()['objectCreated']
+    # Create a Credit Card Account
+    return make_post(url, payload)
 
 def create_bill(accountID):
     url = base_url + '/accounts/{}/bills'.format(accountID)
@@ -85,18 +81,7 @@ def create_bill(accountID):
     }
 
     # Create a bill
-    resp = requests.post( 
-        url, 
-        data=json.dumps(payload),
-        headers={'content-type':'application/json'},
-        params={'key': apiKey}
-        )
-
-    if not resp.ok:
-        print(resp.reason)
-        return resp.json()
-    
-    return resp.json()['objectCreated']
+    return make_post(url, payload)
 
 def create_purchase(accountID):
     url = base_url + '/accounts/{}/purchases'.format(accountID)
@@ -108,39 +93,21 @@ def create_purchase(accountID):
     }
 
     # Create a purchase
-    resp = requests.post( 
-        url, 
-        data=json.dumps(payload),
-        headers={'content-type':'application/json'},
-        params={'key': apiKey}
-        )
-
-    if not resp.ok:
-        print(resp.reason)
-        return resp.json()
-    
-    return resp.json()['objectCreated']
+    return make_post(url, payload)
 
 def create_deposit(accountID):
     url = base_url + '/accounts/{}/deposits'.format(accountID)
     payload = {
         "medium": "balance",
-        "amount": 100.00
+        "amount": 100.00,
+        "status": "completed"
     }
 
     # Create a deposit
-    resp = requests.post( 
-        url, 
-        data=json.dumps(payload),
-        headers={'content-type':'application/json'},
-        params={'key': apiKey}
-        )
+    return make_post(url, payload)
 
-    if not resp.ok:
-        print(resp.reason)
-        return resp.json()
-    
-    return resp.json()['objectCreated']
+def enterprise_get_accounts():
+    pass
 
 def main():
     # # Get all atms within a 2 mile radius of Mclean
